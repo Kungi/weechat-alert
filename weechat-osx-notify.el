40e92942e07437968d9cfe01ef9a970f3905a596
Andreas "Kungi" Klein <git@kungi.org>
2016-03-31 13:19:07 +0200

    Header comment

--- weechat-osx-notify.el
+++ weechat-osx-notify.el
@@ -1,32 +1,36 @@
- ;;; weechat-osx-notify --- OSX Notifications ;; -*- lexical-binding: t -*-
-
+;;; weechat-osx-notify --- OSX Notifications ;; -*- lexical-binding: t -*-
+;;
 ;; Copyright (C) 2016 Andreas Klein
-
+;;
 ;; Author: Andreas Klein <git@kungi.org>
 ;; Keywords: irc chat network weechat
 ;; URL: https://github.com/kungi/weechat-osx-notify.el
 ;; Version: 0.0.1
-
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;;; License:
+;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;;
 ;;; Commentary:
-
+;;
 ;; Weechat.el notifications using OSX system notifications
 ;; Requires terminal-notifier which can be installed via homebrew
 ;;
 ;;     brew install terminal-notifier
-
+;;
 ;;; Code:
 
 (require 'weechat)
