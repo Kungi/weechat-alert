03d3f52d31fe8b9ffb0ebff214ba67413ff2b2df
Andreas "Kungi" Klein <git@kungi.org>
2016-04-16 08:24:15 +0200

    Rename functions and comments

--- weechat-alert.el
+++ weechat-alert.el
@@ -1,10 +1,10 @@
-;;; weechat-osx-notify.el --- Weechat notifier for OSX using terminal-notifier  -*- lexical-binding: t; -*-
+;;; weechat-alert.el --- Weechat notifier using alerts  -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2016 Andreas Klein
 ;;
 ;; Author: Andreas Klein <git@kungi.org>
 ;; Keywords: irc chat network weechat
-;; URL: https://github.com/kungi/weechat-osx-notify
+;; URL: https://github.com/kungi/weechat-alert
 ;; Version: 0.0.1
 ;; Package-Requires: ((weechat "0.3.1") (cl-lib "0.5"))
 ;;
@@ -37,59 +37,59 @@
 (require 'weechat)
 (require 'cl-lib)
 
-(defun weechat-osx-notify-shell-command-maybe (exe &optional paramstr)
+(defun weechat-alert-shell-command-maybe (exe &optional paramstr)
   "run executable EXE with PARAMSTR, or warn if EXE's not available; eg. "
-  " (weechat-osx-notify-shell-command-maybe \"ls\" \"-l -a\")"
+  " (weechat-alert-shell-command-maybe \"ls\" \"-l -a\")"
   (if (executable-find exe)
       (shell-command (concat exe " " paramstr))
     (user-error "'%s' is not found, please install" exe)))
 
-(defun weechat-osx-notify-wrap-in-quotes (s)
+(defun weechat-alert-wrap-in-quotes (s)
   (concat "'" s "'"))
 
 ;; FIXME use shell-quote-argument
-(defun weechat-osx-notify-prepare-params (title text)
+(defun weechat-alert-prepare-params (title text)
   (mapconcat #'identity
              (list "-message"
-                   (weechat-osx-notify-wrap-in-quotes text)
+                   (weechat-alert-wrap-in-quotes text)
                    "-title"
-                   (weechat-osx-notify-wrap-in-quotes title)
+                   (weechat-alert-wrap-in-quotes title)
                    "-group 'weeechat.notification'"
                    "-sender 'org.gnu.Emacs'")
              " "))
 
-(defun weechat-osx-notify-send-osx-notification (title text)
-  (let ((params (weechat-osx-notify-prepare-params title text)))
+(defun weechat-alert-send-osx-notification (title text)
+  (let ((params (weechat-alert-prepare-params title text)))
     (message params)
-    (weechat-osx-notify-shell-command-maybe "terminal-notifier"
+    (weechat-alert-shell-command-maybe "terminal-notifier"
                                             params)))
 
-(defun weechat-osx-notify-handler (type &optional sender text _date buffer-ptr)
+(defun weechat-alert-handler (type &optional sender text _date buffer-ptr)
   (setq text (if text (weechat-strip-formatting text)))
   (setq sender (if sender (weechat-strip-formatting sender)))
   (let ((jump-position (point-max-marker)))
     (let ((text (cl-case type
                   (:highlight
-                   (weechat-osx-notify-send-osx-notification
+                   (weechat-alert-send-osx-notification
                     (format "Highlight from %s"
                             sender)
                     (format "in %s: %s"
                             (weechat-buffer-name buffer-ptr)
                             text)))
                   (:query
-                   (weechat-osx-notify-send-osx-notification
+                   (weechat-alert-send-osx-notification
                     (format "Query from %s"
                             sender)
                     (format "%s"
                             text)))
                   (:disconnect
-                   (weechat-osx-notify-send-osx-notification
+                   (weechat-alert-send-osx-notification
                     "Disconnected from WeeChat"
                     ""))))))))
 
 (add-hook 'weechat-notification-handler-functions
-          'weechat-osx-notify-handler)
+          'weechat-alert-handler)
 
-(provide 'weechat-osx-notify)
+(provide 'weechat-alert)
 
-;;; weechat-osx-notify.el ends here
+;;; weechat-alert.el ends here
